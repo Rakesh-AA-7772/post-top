4a5e0507f2d539f4f6f4eaf3acbe1545ac283583
Rakesh-AA-7772 <rakesh.neon19@gmail.com>
2026-02-07 22:06:48 +0530

    Refactor firebase.js for cleaner structure

--- firebase.js
+++ firebase.js
@@ -1,12 +1,10 @@
 // firebase.js
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import {
   getFirestore,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-firestore.js";
 
-// Your Firebase config (from console)
 const firebaseConfig = {
   apiKey: "AIzaSyCibuFclmHoonkZqVOT3vOXmHHUfsK8hbo",
   authDomain: "posttop-7155a.firebaseapp.com",
@@ -16,11 +14,7 @@ const firebaseConfig = {
   appId: "1:975294540512:web:9f4bf8e26b26e6e8e47e85"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
 const db = getFirestore(app);
 
-// Export what the app needs
 export { db, serverTimestamp };
